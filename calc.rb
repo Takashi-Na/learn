7ad4baf0e07cf486f64b893ae277edd8d2f91eb0
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-08-28 23:02:45 +0900

    update calc

--- calc.rb
+++ calc.rb
@@ -1,18 +1,19 @@
-# 標準入力
-input_order = gets.chomp.split("'")
+class calc < unit
+  # 標準入力
+  input_order = gets.chomp.split("'")
 
-# 命令と式を変数に代入
-order = input_order[0]
-formula = input_order[1]
+  # 命令と式を変数に代入
+  order = input_order[0]
+  formula = input_order[1]
 
-p order
-p formula
+  p order
+  p formula
 
-# 単位を数字に変換
-# 別クラスを用いて変換するのが好ましい
-if formula.include?("mg")
-  
+  # 単位を数字に変換
+  # 別クラスを用いて変換するのが好ましい
+  if formula.include?("mg")
+    
+  end
+
+  # *,/を優先して計算しその後に+,-の計算を行う
 end
-
-# *,/を優先して計算しその後に+,-の計算を行う
-
